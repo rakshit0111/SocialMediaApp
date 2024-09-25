507677f99ab4c39a31a09e31ae8ac0bac176287e
rakshit0111 <rakshitsingh333@gmail.com>
2024-09-26 01:20:15 +0530

    Basic Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Main from './pages/Main';
+import Login from './pages/Login';
+import Navbar from './Components/Navbar';
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Main/>}/>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Main.tsx
@@ -0,0 +1,7 @@
+const Main = () => {
+    return (<div>
+      MAIN
+    </div>)
+}
+
+export default Main;
