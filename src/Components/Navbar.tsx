a6f7fed6bd73c1b8f906cd0bcc5d27d8f75fbbe7
rakshit0111 <rakshitsingh333@gmail.com>
2024-09-26 20:38:03 +0530

    some more additions in auth

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,16 +1,31 @@
 import {Link} from 'react-router-dom'
 import { auth } from '../Config/firebase';
 import {useAuthState} from 'react-firebase-hooks/auth'
+import {signOut} from 'firebase/auth';
+import {useNavigate} from 'react-router-dom';
 
 const Navbar = () => {
     const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+    const signUserOut = async () => {
+        await signOut(auth);
+        navigate("/login");
+    }
     return <div>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login</Link>
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+        </div>
+       
 
         <div>
-            <p>{user?.displayName}</p>
-            <img src={user?.photoURL || ""} width="100" height="100"/>
+            {
+                user && <>
+                <p>{user?.displayName}</p>
+                <img src={user?.photoURL || ""} width="100" height="100"/>
+                <button onClick={signUserOut}>Log Out</button>
+            </>  
+            }
         </div>
     </div>
 };
